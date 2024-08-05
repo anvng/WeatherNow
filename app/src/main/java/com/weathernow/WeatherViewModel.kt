327d538cf22991ffee5f1a30eb2728bab85f1dec
An Van Nguyen <152575252+anvng@users.noreply.github.com>
2024-08-05 17:20:56 +0700

    feat: called Api vs UI wit data

--- app/src/main/java/com/weathernow/WeatherViewModel.kt
+++ app/src/main/java/com/weathernow/WeatherViewModel.kt
@@ -1,10 +1,38 @@
 package com.weathernow
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.weathernow.api.Constant
+import com.weathernow.api.NetworkResponse
+import com.weathernow.api.RetrofitInstance
+import com.weathernow.api.WeatherModel
+import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
+
+    private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherData = MutableLiveData<NetworkResponse<WeatherModel>>()
+    val weatherData: LiveData<NetworkResponse<WeatherModel>> = _weatherData
+
+
     fun getWeather(city: String) {
-        Log.i("WeatherViewModel", "Getting weather for $city")
+        _weatherData.value = NetworkResponse.Loading
+        viewModelScope.launch {
+            try {
+                val response = weatherApi.getWeather(city, Constant.weatherApiKey)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _weatherData.value = NetworkResponse.Success(it)
+                    }
+                } else {
+                    _weatherData.value = NetworkResponse.Error("Failed to fetch weather data!")
+
+                }
+            }catch (e: Exception){
+                _weatherData.value = NetworkResponse.Error("Failed to fetch weather data!")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/weathernow/api/WeatherModel.kt
@@ -0,0 +1,6 @@
+package com.weathernow.api
+
+data class WeatherModel(
+    val current: Current,
+    val location: Location
+)
